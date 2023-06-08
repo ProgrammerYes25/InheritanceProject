4920cf813d2f122d694ad4d5d6518725141b6dc4
yes25 <programmer.yes25@gmail.com>
2023-06-08 14:45:17 +0900

    is와 as 키워드

---
+++ InheritanceProject/Dog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace InheritanceProject
+{
+    internal class Dog : Animal
+    {
+        public string Color { get; set; }
+
+        public void Bark() { Console.WriteLine("왈왈 짓습니다."); }
+
+    }
+}
--- InheritanceProject/Program.cs
+++ InheritanceProject/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,30 @@ namespace InheritanceProject
     {
         static void Main(string[] args)
         {
+            List<Animal> Animal = new List<Animal>() { new Dog(), new Dog(), new Dog(),  new Cat(), new Cat() , new Cat() );
+
+            foreach(var item in Animal)
+            {
+                item.Eat();
+                item.Sleep();
+
+                Dog dog = item as Dog;
+                if (dog != null)
+                {
+                    dog.Bark();
+                }
+
+
+                if (item is Dog)
+                {
+                    ((Dog)item).Bark();
+                }
+                else if (item is Cat) {
+                    ((Cat)item).Meow();
+                }
+                
+                
+            }
         }
-    }
+    } 
 }
